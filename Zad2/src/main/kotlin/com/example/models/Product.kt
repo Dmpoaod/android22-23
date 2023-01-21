4f9012e6d412cc70b4e874477596eee4fcc980f0
Michał Wilewski <55109192+Dmpoaod@users.noreply.github.com>
2023-01-21 17:37:02 +0100

    Add files via upload

--- Zad2/src/main/kotlin/com/example/models/Product.kt
+++ Zad2/src/main/kotlin/com/example/models/Product.kt
@@ -1,16 +1,22 @@
 package com.example.models
 
 
+import com.google.api.client.json.Json
+import com.google.common.net.MediaType.parse
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 
+
 data class Product(
-    val id : Int? = null,
-    val name : String,
-    val qty : Int,
-    val price : Double,
-    val type : String
+    val id: Int? = null,
+    val name: String,
+    val qty: Int,
+    val price: Double,
+    val type: String
 )
 
 
@@ -33,3 +39,25 @@ object Products : Table() {
         )
 
 }
+
+fun parseProduct(json: String): Product {
+    val jsonObject = Json.parse(json).jsonObject
+    val product = Product(
+        id = jsonObject["id"].Int,
+        name = jsonObject["name"].content,
+        price = jsonObject["price"].double,
+        type = jsonObject["type"].String,
+        qty = jsonObject["qty"].Int
+    )
+    return product
+}
+
+suspend fun downloadProduct(productId: String?): Product {
+    val client = HttpClient()
+    val url = "https://yourserver.com/products/$productId"
+    val response = client.get<HttpResponse>(url)
+    val json = response.readText()
+    client.close()
+    return parseProduct(json)
+}
+
