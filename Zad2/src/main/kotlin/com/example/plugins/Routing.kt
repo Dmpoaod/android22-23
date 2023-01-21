a15ea3ba1c636e4f50f540b3f4390eac8bb60a9c
Michał Wilewski <55109192+Dmpoaod@users.noreply.github.com>
2023-01-21 17:37:45 +0100

    Add files via upload

--- Zad2/src/main/kotlin/com/example/plugins/Routing.kt
+++ Zad2/src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,6 +4,7 @@ package com.example.plugins
 import com.example.functions.PaymentData
 import com.example.functions.mockPayment
 import com.example.models.Product
+import com.example.models.downloadProduct
 import com.google.gson.Gson
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -22,6 +23,12 @@ fun Application.configureRouting() {
             call.respondText(gson.toJson(x))
         }
 
+        get("/download/{productId}") {
+            val productId = call.parameters["productId"]
+            val product = downloadProduct(productId)
+            call.respond(product)
+        }
+
         get("/pay") {
             val paymentData : PaymentData = (PaymentData("1234567890625142", "03/24", "111", 1546.00))
             val isPaymentSuccessful = mockPayment(paymentData)
@@ -33,3 +40,4 @@ fun Application.configureRouting() {
         }
     }
 }
+
