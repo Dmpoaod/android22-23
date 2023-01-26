6c59ef3d5f33b1075c3b159610e7cd4877ea36ba
Michał Wilewski <55109192+Dmpoaod@users.noreply.github.com>
2023-01-26 15:16:36 +0100

    Add files via upload

--- Zad2/src/main/kotlin/com/example/Application.kt
+++ Zad2/src/main/kotlin/com/example/Application.kt
@@ -1,29 +1,32 @@
 package com.example
 
+import com.example.models.Product
 import com.example.models.User
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.jetty.*
 import com.example.plugins.*
+import com.google.api.services.storage.model.Notifications
 import config.firebase.FirebaseAdmin
+import io.ktor.client.*
+import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.apache.http.client.methods.RequestBuilder.head
 import org.slf4j.event.Level
+import com.pusher.pushnotifications.PushNotifications;
 
 fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
 
-fun Application.module() {
-    // initialize Firebase Admin SDK
+fun Application.main() {
     FirebaseAdmin.init()
 
     install(ContentNegotiation) { gson { setPrettyPrinting() } }
-    install(Authentication) { firebase { configure() } }
+
     install(CallLogging) {
         level = Level.INFO
         filter { call -> call.request.path().startsWith("/") }
@@ -45,4 +48,18 @@ fun Application.module() {
     configureMonitoring()
     configureSerialization()
     configureRouting()
+
+    val client = HttpClient() {
+        install(Notifications) {
+            onSuccess { call ->
+                val product = call.response.receive<Product>()
+            }
+        }
+    }
+    val newProducts = client.get<List<Product>>("https://your-server.com/new-products")
+    for (product in newProducts) {
+        PushNotifications.start(getApplicationContext(), "Nowy Produkt: $product");
+    }
+    client.close()
 }
+
